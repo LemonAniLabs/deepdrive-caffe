s9xie/caffe

[pycaffe] re-expose SGDSolver, and expose other solvers

python/caffe/__init__.py
@@ -1,6 +1,6 @@
 from .pycaffe import Net, SGDSolver
 from ._caffe import set_mode_cpu, set_mode_gpu, set_phase_train, \
-        set_phase_test, set_device, Layer
+        set_phase_test, set_device, Layer, get_solver
 from .classifier import Classifier
 from .detector import Detector
 import io

LemonAniLabs/deepdrive-caffe

python/caffe/__init__.py
@@ -1,6 +1,6 @@
 from .pycaffe import Net, SGDSolver
 from ._caffe import set_mode_cpu, set_mode_gpu, set_device, \
-    set_phase_train, set_phase_test, Layer
+    set_phase_train, set_phase_test, Layer, get_solver
 from .classifier import Classifier
 from .detector import Detector
 import io